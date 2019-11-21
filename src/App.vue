5421014e9a6253418413b55b83e9865d632f3b23
Sodavin Chheng <sodavinchheng@Sodavins-MacBook-Pro.local>
2019-11-21 21:41:23 +0700

    Created NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <NavBar />
 
     <v-content>
-      <HelloWorld/>
+      <router-link to="/" replace>Home</router-link> | 
+      <router-link to="/about" replace>About</router-link>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import NavBar from './components/NavBar'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    NavBar
   },
 
   data: () => ({
@@ -58,3 +25,10 @@ export default {
   }),
 };
 </script>
+
+<style>
+  *{
+    margin: 0px;
+    padding: 0px;
+  }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,71 @@
+<template>
+    <nav>
+        <v-app-bar app color="primary" dark>
+            
+            <v-btn text>
+                <v-icon v-on:click="drawer = !drawer">mdi-menu</v-icon>
+            </v-btn>
+
+            <div class="d-flex align-center">
+                <v-img
+                alt="Zeal Logo"
+                class="shrink mx-4"
+                contain
+                src="../assets/Zeal-Logo.png"
+                transition="scale-transition"
+                width="75"
+                />
+
+                <h1 class="hidden-sm-and-down">Online Remote Classroom</h1>
+
+            </div>
+
+            <v-spacer></v-spacer>
+            
+            <v-btn text>
+                <v-icon>mdi-plus</v-icon>
+            </v-btn>
+
+            <v-btn text>
+                <v-icon>mdi-bell</v-icon>
+            </v-btn>
+
+            <v-btn text>
+                <v-icon>mdi-exit-to-app</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" color="primary" dark width="400px" class="mt-12 d-lg-flex">
+            <v-list class="mt-3">
+                <v-list-item v-for="subject in subjects" :key="subject.name" route :to="subject.route" replace>
+                    <v-list-item-icon>
+                        <v-icon>{{subject.icon}}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">{{ subject.name }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    data() {
+        return {
+            drawer: true,
+            subjects:[
+                {name: 'Artificial Intelligence', icon: 'mdi-brain', route: '/ai'},
+                {name: 'Cryptography & Network Security', icon: 'mdi-security', route: '/cns'},
+                {name: 'Data Structures & Algorithms', icon: 'mdi-graph', route: '/dsa'},
+                {name: 'Database Management System', icon: 'mdi-database', route: '/dbms'},
+                {name: 'Growth Hacking', icon: 'mdi-trending-up', route: '/gh'},
+                {name: 'Leadership & Communication', icon: 'mdi-voice', route: '/lc'},
+                {name: 'Web App Development', icon: 'mdi-web', route: '/wad'}
+            ]
+        }
+    }
+}
+</script>
